6ee4f1ce1ff43ee6b9e1df75f7177e53b004d78d
KetyaNop <cnop@princeton.edu>
2023-12-10 14:20:43 -0500

    Add main light

--- src/lights/BasicLights.ts
+++ src/lights/BasicLights.ts
@@ -1,4 +1,4 @@
-import { Group, SpotLight, AmbientLight, HemisphereLight } from 'three';
+import { Group, SpotLight, AmbientLight, HemisphereLight, DirectionalLight} from 'three';
 
 class BasicLights extends Group {
     constructor() {
@@ -6,13 +6,16 @@ class BasicLights extends Group {
         super();
 
         const dir = new SpotLight(0xffffff, 55, 7, 0.8, 1, 1);
-        const ambi = new AmbientLight(0x404040, 1.32);
+        const ambi = new AmbientLight('white', 5);
+        const mainLight = new DirectionalLight('white', 10);
+            
         const hemi = new HemisphereLight(0xffffbb, 0x080820, 2.3);
 
-        dir.position.set(0, 1, 0);
+        mainLight.position.set(10, 10, 10);
+        dir.position.set(0, 2, 0);
         dir.target.position.set(0, -3, 0);
 
-        this.add(ambi, hemi, dir);
+        this.add(ambi, hemi, dir, mainLight);
     }
 }
 
